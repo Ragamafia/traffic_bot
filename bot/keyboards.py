9857582123d3b2d6f52e68ef49e4d4bd14fae182
Иван Тоцких <sales3-suzuki@terra.local>
2024-12-05 14:35:51 +0800

    save change

--- bot/keyboards.py
+++ bot/keyboards.py
@@ -1,16 +1,10 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-start_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text='Запросить траффик'),
-            KeyboardButton(text='Связаться с разработчиком'),
-        ],
-        [
-            KeyboardButton(text='Канал мастерской KARBAN'),
-        ],
-    ],
-    resize_keyboard=True,
-    input_field_placeholder='Выберите раздел',
-)
+def links():
+    link_list = [
+        [InlineKeyboardButton(text='Запросить траффик ✈', url='https://t.me/karban_flo')],
+        [InlineKeyboardButton(text='Связаться с разработчиком', url='https://t.me/raga_mafia')],
+        [InlineKeyboardButton(text='Перейти на канал мастерской KARBAN', url='https://t.me/karban_admin')]
+    ]
+    return InlineKeyboardMarkup(inline_keyboard=link_list, resize_keyboard=True)
