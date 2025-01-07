6ee2c4a27ab47cd8743cfe619155e0464b907be0
Иван Тоцких <sales3-suzuki@terra.local>
2025-01-07 16:51:38 +0800

    edit README

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,8 @@ import config as cfg
 
 def generate_message(filtered_flights):
     total = len(filtered_flights["arrival"]) + len(filtered_flights["departure"])
-    if total >= cfg.compare_flights:
 
+    if total >= cfg.compare_flights:
         compare_word, message = "более", "Возможно затруднение движения"
     else:
         compare_word, message = "менее", "Дорога у Аэропорта свободна"
